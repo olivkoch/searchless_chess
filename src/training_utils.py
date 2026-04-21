123cd8d34ad788231b1eb4ced337c41a4234356e
Olivier Koch <olivier.koch@gmail.com>
2026-04-21 09:54:39 +0200

    defer eval of jax sharding

--- src/training_utils.py
+++ src/training_utils.py
@@ -35,7 +35,7 @@ from searchless_chess.src import constants
 
 def replicate(
     array_tree: chex.ArrayTree,
-    sharding: jax.sharding.PositionalSharding,
+    sharding: 'jax.sharding.PositionalSharding',
 ) -> chex.ArrayDeviceTree:
   """Replicates the `array_tree` across all devices specified by `sharding`.
 
@@ -194,7 +194,7 @@ def restore_checkpoint(
     params_ema: hk.Params,
     opt_state: optax.OptState,
     data_iter: pygrain.PyGrainDatasetIterator,
-    sharding: jax.sharding.PositionalSharding,
+    sharding: 'jax.sharding.PositionalSharding',
 ) -> tuple[
     hk.Params, hk.Params, optax.OptState, pygrain.PyGrainDatasetIterator
 ]:
